684a2169de3aae94382bc949f25a7f5e062de107
Ann Philip <aphilip@uoregon.edu>
2020-04-10 16:39:19 -0700

    Add arrAvg to functions.js.

---
+++ javascripts/functions.js
@@ -0,0 +1,17 @@
+// jshint esversion: 7
+
+// Req. 3b
+let arrAvg = (arr) => {
+  let sum = 0, average;
+
+  for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  average = sum/arr.length;
+  console.log(average);
+};
+
+//Req. 3c
+let arrMax = (arr) {
+
+};
